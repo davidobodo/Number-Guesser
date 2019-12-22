8e4d9e8188439006759f7b6ca78280484e0485ea
David Obodo <obododavid5@gmail.com>
2019-12-22 19:13:33 +0100

    get players name

--- Program.cs
+++ Program.cs
@@ -15,6 +15,12 @@ namespace NumberGuesser
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
             Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game", input);
         }
     }
 }
