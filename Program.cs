7e92c1c5f9a8de0478cc9be133a32c2e27869e53
David Obodo <obododavid5@gmail.com>
2019-12-22 18:07:36 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Obodo David";
+            int age = 24;
+
+            Console.WriteLine(name + " is " + age);
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
