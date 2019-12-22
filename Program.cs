c98771d2f0780f123b7953ee0edd7e4affcaf43f
David Obodo <obododavid5@gmail.com>
2019-12-22 19:10:23 +0100

    Change color of text

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            string appName = "number Guesser";
+            string appName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "Obodo David";
 
+            Console.ForegroundColor = ConsoleColor.Green;
+
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
         }
     }
 }
