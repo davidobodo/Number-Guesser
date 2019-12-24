461324ea08bb6e5248539a135c1326c8300f0394
David Obodo <obododavid5@gmail.com>
2019-12-24 15:27:13 +0100

    outsource app info into seperate function

--- Program.cs
+++ Program.cs
@@ -6,83 +6,54 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            GetAppInfo();
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+            Console.WriteLine("Hello {0}, let's play a game", inputName);
+            while (true)
+            {
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guess = 0;
+                Console.WriteLine("Guess a number between 1 and 10");
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Sorry, please enter an actual number");
+                        Console.ResetColor();
+                        continue;
+                    }
+                    guess = Int32.Parse(input);
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong number, please try again");
+                        Console.ResetColor();
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("You are correct!!!");
+                Console.ResetColor();
+                Console.WriteLine("Play Again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+                if( answer == "Y" ){ continue; }
+                else if ( answer == "N"){return; }
+                else{ return; }
+            }
+        }
+
+        static void GetAppInfo()
+        {
             string appName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "Obodo David";
 
             Console.ForegroundColor = ConsoleColor.Green;
-
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
-
             Console.ResetColor();
-
-            Console.WriteLine("What is your name?");
-
-            string inputName = Console.ReadLine();
-
-            Console.WriteLine("Hello {0}, let's play a game", inputName);
-
-            while (true)
-            {
-
-                Random random = new Random();
-
-                int correctNumber = random.Next(1, 10);
-
-                int guess = 0;
-
-                Console.WriteLine("Guess a number between 1 and 10");
-
-                while (guess != correctNumber)
-                {
-                    string input = Console.ReadLine();
-
-                    if (!int.TryParse(input, out guess))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Sorry, please enter an actual number");
-
-                        Console.ResetColor();
-
-                        continue;
-                    }
-
-                    guess = Int32.Parse(input);
-
-                    if (guess != correctNumber)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Wrong number, please try again");
-
-                        Console.ResetColor();
-                    }
-                }
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-
-                Console.WriteLine("You are correct!!!");
-
-                Console.ResetColor();
-
-                Console.WriteLine("Play Again? [Y or N]");
-
-                string answer = Console.ReadLine().ToUpper();
-
-                if(answer == "Y")
-                {
-                    continue;
-                }else if (answer == "N")
-                {
-                    return;
-                }
-                else
-                {
-                    return;
-                }
-
-            }
         }
     }
 }
